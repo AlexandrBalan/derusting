4407594de2349c6272999059db2bbecb070ef112
AlexandrBalan <epix246@gmail.com>
2026-05-01 23:58:08 -0700

    completed the listpractice

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,26 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
-
+    List<String> groceries = new ArrayList<>();
     // Add 3 elements to the list (OK to do one-by-one)
-
+    groceries.add("tomatoes");
+    groceries.add("snacks");
+    groceries.add("protein powder");
     // Print the element at index 1
-
+    System.out.println(groceries.get(1));
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
-
+    System.out.println(groceries.set(1, "best snacks"));
+    System.out.println(groceries);
     // Insert a new element at index 0 (the length of the list will change)
-
+    groceries.add(0, "oranges");
+    System.out.println(groceries);
     // Check whether the list contains a certain string
-
+    System.out.println(groceries.contains("oranges"));
     // Iterate over the list using a traditional for-loop.
+    for(int i = 0; i < groceries.size(); i++) {
+      System.out.println(groceries.get(i));
+    }
     // Print each index and value on a separate line
-
+    for(int i = 0; i < groceries.size(); i++ ) {
+      System.out.println(groceries.get(i) + " is at index " + i);
+    }
     // Sort the list using the Collections library
-
+    Collections.sort(groceries);
+    System.out.println(groceries);
     // Iterate over the list using a for-each loop
+    System.out.println("-------------------------------------------------------------");
+    for(String i : groceries) {
+      System.out.println(i);
+    }
     // Print each value on a second line
 
     /*
